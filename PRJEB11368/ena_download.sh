be3a66947f70f30de7ed4726e82d22f52511e47e
bryshalm <bryshalm@gmail.com>
2026-03-29 22:34:05 -0700

    new dada2 rmd

---
+++ PRJEB11368/ena_download.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR105/005/ERR1056275/ERR1056275.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR105/004/ERR1056264/ERR1056264.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR105/006/ERR1056256/ERR1056256.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR105/009/ERR1056269/ERR1056269.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR105/002/ERR1056272/ERR1056272.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR105/000/ERR1056270/ERR1056270.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR105/007/ERR1056267/ERR1056267.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR105/001/ERR1056261/ERR1056261.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR105/005/ERR1056265/ERR1056265.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR105/004/ERR1056274/ERR1056274.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR105/009/ERR1056259/ERR1056259.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR105/003/ERR1056263/ERR1056263.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR105/008/ERR1056268/ERR1056268.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR105/007/ERR1056257/ERR1056257.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR105/000/ERR1056260/ERR1056260.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR105/001/ERR1056271/ERR1056271.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR105/006/ERR1056266/ERR1056266.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR105/003/ERR1056273/ERR1056273.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR105/008/ERR1056258/ERR1056258.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR105/002/ERR1056262/ERR1056262.fastq.gz
---
+++ PRJNA302730/ena_download.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR294/001/SRR2944981/SRR2944981.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR294/002/SRR2944992/SRR2944992.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR294/007/SRR2944997/SRR2944997.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR294/009/SRR2944989/SRR2944989.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR294/004/SRR2944984/SRR2944984.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR294/005/SRR2944995/SRR2944995.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR294/000/SRR2945000/SRR2945000.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR294/000/SRR2944990/SRR2944990.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR294/009/SRR2944999/SRR2944999.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR294/004/SRR2944994/SRR2944994.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR294/006/SRR2944986/SRR2944986.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR294/008/SRR2944988/SRR2944988.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR294/003/SRR2944993/SRR2944993.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR294/000/SRR2944980/SRR2944980.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR294/005/SRR2944985/SRR2944985.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR294/009/SRR2944979/SRR2944979.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR294/002/SRR2944982/SRR2944982.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR294/001/SRR2945001/SRR2945001.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR294/006/SRR2944996/SRR2944996.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR294/003/SRR2944983/SRR2944983.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR294/002/SRR2945002/SRR2945002.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR294/008/SRR2944998/SRR2944998.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR294/007/SRR2944987/SRR2944987.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR294/001/SRR2944991/SRR2944991.fastq.gz
+
