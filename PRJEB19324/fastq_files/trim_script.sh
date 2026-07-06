50e038e85f2e4cc0e465a85c3a3e5cefe725e448
bryshalm <bryshalm@gmail.com>
2026-07-06 11:42:22 -0700

    trimmed files

---
+++ PRJEB19324/fastq_files/trim_script.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+mkdir -p trimmed
+
+for FQ in *.fastq.gz; do
+    SAMPLE="${FQ/.fastq.gz/}"
+
+    cutadapt \
+        -g CCTACGGGAGGCAGCAG \
+        -a GACTACNVGGGTWTCTAAT \
+        --discard-untrimmed \
+        -o trimmed/${SAMPLE}_trimmed.fastq.gz \
+        "$FQ" \
+        -j 4 \
+        > trimmed/${SAMPLE}_cutadapt.log 2>&1
+
+    echo "Done: $SAMPLE"
+done
